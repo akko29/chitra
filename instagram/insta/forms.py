372d0066ce6ec89ca1a2f542fddeb200ed910430
mudit8560 <mudit.1996garg@gmail.com>
2017-11-12 03:03:25 +0530

    new changes admin

--- instagram/insta/forms.py
+++ instagram/insta/forms.py
@@ -12,3 +12,14 @@ class UserRegistrationForm(forms.Form):
 	# 	data = self.cleaned_data
 	# 	user = User.object.create(username=data['username'],first_name = data['first'],last_name = data['last'],email= data['email'],pass = data['password'])
 	# 	user.save()
+	from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+
+class SignupForm(UserCreationForm):
+    email = forms.EmailField(max_length=200, help_text='Required')
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2')
